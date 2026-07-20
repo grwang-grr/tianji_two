d1484ed5ca5100a34bab57f4fdebc64e331a0b81
wg <wang456r@qq.com>
2026-07-20 13:04:45 +0800

    增加了优惠劵作用范围功能

---
+++ tj-promotion/src/main/java/com/tianji/promotion/controller/CouponScopeController.java
@@ -0,0 +1,15 @@
+package com.tianji.promotion.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 优惠券作用范围信息 前端控制器
+ */
+@RestController
+@RequestMapping("/coupon-scope")
+public class CouponScopeController {
+
+}
